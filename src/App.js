07731d26e82aa5c7b00b68d79d5886c46635c7a5
Rodrigo Reis <roreis99@gmail.com>
2019-10-05 16:58:56 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,8 @@
 import React from 'react';
 
-import styled from 'styled-components';
-
-const Wrapper = styled.div`
-  width: 100%;
-  height: 100vh;
-  color: #fff;
-  font-weight: bold;
-  text-transform: uppercase;
-  display: flex;
-  justify-content: center;
-  background-color: #202c37;
-  h1 {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    letter-spacing: 1px;
-    font-size: 32px;
-    font-family: 'roboto';
-  }
-`;
-
 const App = () => (
   <Wrapper>
-    <h1>Netinho Basic React App </h1>
+    <h1>Basic React App </h1>
   </Wrapper>
 );
 export default App;
